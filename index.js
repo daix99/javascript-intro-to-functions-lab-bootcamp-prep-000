05bf19acdb07d7867583f2cfed675f65aa102107
Daix Ur <d.urnikyte@gmail.com>
2018-01-13 13:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string('I can\'t hear you'.toLowerCase())
+  return string('I can\'t hear you').toLowerCase()
 }
