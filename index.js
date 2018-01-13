0ed3ccdc3548492ed56c41c640660cc11e8af237
Daix Ur <d.urnikyte@gmail.com>
2018-01-13 14:31:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string('I can\'t hear you!'.toLowerCase())
-}
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"; } 
+    
+    else if (string.toUpperCase() === string) {
+    return "YES INDEED!";}
+
+    else if (string === "I love you, Grandma.") {
+    return "I love you, too.";}
