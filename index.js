3edc9590c667fed8c5479cdb6182960490ba422e
Daix Ur <d.urnikyte@gmail.com>
2018-01-13 13:24:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout (string) {
+  console.log.toUpperCase()
+}
+
+function logShout(string) {
+  console.log.toLowerCase()
+}
+
+function sayHiToGrandma(string){
+  console.console.log("I can\'t hear you!").toLowerCase()
+}
